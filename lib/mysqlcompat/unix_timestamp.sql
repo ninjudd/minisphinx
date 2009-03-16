ebc9042a1ec1539b49f77647d8f0bee0fb38ceae
Justin Balthrop <justin@geni.com>
2009-03-16 13:16:05 -0700

    add mysqlcompat functions from ultrasphinx

---
+++ lib/mysqlcompat/unix_timestamp.sql
@@ -0,0 +1,13 @@
+/* Taken from ultrasphinx */
+
+/* 
+ mysqlcompat-1.0b3
+ public domain
+ modified
+ UNIX_TIMESTAMP(date)
+*/
+
+CREATE FUNCTION unix_timestamp(timestamp without time zone)
+RETURNS bigint AS $$
+  SELECT EXTRACT(EPOCH FROM $1)::bigint
+$$ VOLATILE LANGUAGE SQL;
